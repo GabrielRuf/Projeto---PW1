47ce9f3a28fbe83a94807d38300c505d727b1e0f
Gabriel <gabriel.rufino05@gmail.com>
2022-10-08 16:49:12 -0300

    att imoveis

--- Imoveis.js
+++ Imoveis.js
@@ -1,43 +1,40 @@
 let imoveis = []
 let i = 0
 
-function CadastraImoveis() 
-{
+function CadastraImoveis() {
   event.preventDefault()
 
   let imovel = {
-    codigo : document.getElementById("code").value,
-    description: document.getElementById("description").value,
-    rua: document.getElementById("rua").value,
-    num: document.getElementById("num").value,
-    bairro: document.getElementById("bairro").value,
-    city: document.getElementById("city").value,
-    estado: document.getElementById("UF").value,
-    pais: document.getElementById("pais").value,
-    cep: document.getElementById("cep").value,
-    tipo: document.getElementById("tipo").value,
-    garagens: document.getElementById("garagens").value,
-    suites: document.getElementById("suite").value,
-    quartos: document.getElementById("quartos").value,
-    seguranca: document.getElementById("seguranca").value,
-    preco: document.getElementById("preco").value,
-    condominio: document.getElementById("condominio").value
-  };
-  
-  Cadastro = BuscarCadastro(imoveis,imovel.codigo);
-  
-  if (Cadastro == -1) 
-  {
-    imoveis.push(imovel);
-    alert('Imóvel cadastrada !');
-  } 
-  else {
-    alert('Imóvel já cadastrado !');
+    codigo: document.getElementById('code').value,
+    description: document.getElementById('description').value,
+    rua: document.getElementById('rua').value,
+    num: document.getElementById('num').value,
+    bairro: document.getElementById('bairro').value,
+    city: document.getElementById('city').value,
+    UF: document.getElementById('UF').value,
+    pais: document.getElementById('pais').value,
+    cep: document.getElementById('cep').value,
+    tipo: document.getElementById('tipo').value,
+    garagens: document.getElementById('garagens').value,
+    suites: document.getElementById('suite').value,
+    quartos: document.getElementById('quartos').value,
+    seguranca: document.getElementById('seguranca').value,
+    preco: document.getElementById('preco').value,
+    condominio: document.getElementById('condominio').value
+  }
+
+  Cadastro = BuscarCadastro(imoveis, imovel.codigo)
+
+  if (Cadastro == -1) {
+    imoveis.push(imovel)
+    alert('Imóvel cadastrado !')
+  } else {
+    alert('Imóvel já cadastrado !')
   }
 }
 function ListarTodos() {
   let linhaCadastro =
-    '<table><tr><th>Código</td><th>Descrução</td>Rua<th>Número</td><th>Bairro</td><th>Cidade</td><th>Estado</td><th>País</td><th>CEP</td><th>Garagens</td><th>Suites</td></th>Suítes</td><th>Quartos</td><th>segurança</td><th>Preço</td><th>condominio</td></tr></td></tr>'
+    '<table><tr><th>Código</td><td>Descrição</td><td>Rua</td><td>Número</td><td>Bairro</td><td>Cidade</td><td>Estado</td><td>País</td><td>CEP</td><td>Garagens</td><td>Suites</td><td>Suítes</td><td>Quartos</td><td>segurança</td><td>Preço</td><td>condominio</td></th></tr>'
   let i = 0
   if (imoveis.length > 0) {
     for (i = 0; i < imoveis.length; i++) {
@@ -100,9 +97,9 @@ function ListarTodos() {
 }
 
 function ListarEspecifico() {
-  let codigoEspecifico = document.getElementById("codigolistar").value;
+  let codigoEspecifico = document.getElementById('codigolistar').value
   let linhaCadastro =
-    '<table><tr><th>Código</td><th>Descrição</td>Rua<th>Número</td><th>Bairro</td><th>Cidade</td><th>Estado</td><th>País</td><th>CEP</td><th>Garagens</td><th>Suites</td></th>Suítes</td><th>Quartos</td><th>segurança</td><th>Preço</td><th>condominio</td></tr></td></tr>'
+  '<table><tr><th>Código</td><td>Descrição</td><td>Rua</td><td>Número</td><td>Bairro</td><td>Cidade</td><td>Estado</td><td>País</td><td>CEP</td><td>Garagens</td><td>Suites</td><td>Suítes</td><td>Quartos</td><td>segurança</td><td>Preço</td><td>condominio</td></th></tr>'
   if (imoveis.length > 0) {
     Cadastro = BuscarCadastro(imoveis, codigoEspecifico)
     if (Cadastro != -1) {
@@ -167,18 +164,81 @@ function ListarEspecifico() {
   }
 }
 
-let DadoparaAlterar = ''
-let possivelAlterar = false
-let ImoveisAlterando = -1
+let DadoparaAlterar = '';
+let possivelAlterar = false;
+let ImoveisAlterando = -1;
 
-function BuscarCadastro(VetorImovel, codigo) {
+
+function pesquisar() {
+  let codigoEspecifico = document.getElementById('codigoalterar').value
+  let linhaCadastro =
+  '<table><tr><th>Código</td><td>Descrição</td><td>Rua</td><td>Número</td><td>Bairro</td><td>Cidade</td><td>Estado</td><td>País</td><td>CEP</td><td>Garagens</td><td>Suites</td><td>Suítes</td><td>Quartos</td><td>segurança</td><td>Preço</td><td>condominio</td></th></tr>'
+
+  if (imoveis.length > 0) {
+    Cadastro = BuscarCadastro(imoveis, codigoEspecifico)
+    if (Cadastro != -1) {
+      linhaCadastro +=
+        '<tr><td>' +
+        imoveis[Cadastro].codigo +
+        '</td>' +
+        '<td>' +
+        imoveis[Cadastro].description +
+        '</td>' +
+        '<td>' +
+        imoveis[Cadastro].rua +
+        '</td>' +
+        '<td>' +
+        imoveis[Cadastro].bairro +
+        '</td>' +
+        '<td>' +
+        imoveis[Cadastro].city +
+        '</td>' +
+        '<td>' +
+        imoveis[Cadastro].UF +
+        '</td>' +
+        '<td>' +
+        imoveis[Cadastro].pais +
+        '</td>' +
+        '<td>' +
+        imoveis[Cadastro].cep +
+        '</td>' +
+        '<td>' +
+        imoveis[Cadastro].garagens +
+        '</td>' +
+        '<td>' +
+        imoveis[Cadastro].suites +
+        '</td>' +
+        '<td>' +
+        imoveis[Cadastro].quartos +
+        '</td>' +
+        '<td>' +
+        imoveis[Cadastro].seguranca +
+        '</td>' +
+        '<td>' +
+        imoveis[Cadastro].preco +
+        '</td>' +
+        '<td>' +
+        imoveis[Cadastro].condominio +
+        '</td></tr></td></tr>'
+      linhaCadastro += '</table>'
+      document.getElementById('tabela3').innerHTML = linhaCadastro
+      ImoveisAlterando = Cadastro;
+    } else {
+      document.getElementById('tabela3').innerHTML = 'Cliente não encontrado !'
+    }
+  } else {
+    document.getElementById('tabela3').innerHTML = 'Nenhum Cliente cadastrado !'
+  }
+}
+
+function BuscarCadastro(VetorImovel, CODIGO) {
   let i = 0
   for (i = 0; i < VetorImovel.length; i++) {
-    if (VetorImovel[i].codigo == codigo) {
-      return i;
+    if (VetorImovel[i].codigo == CODIGO) {
+      return i
     }
   }
-  return -1;
+  return -1
 }
 
 function checkedOnClick(el) {
@@ -196,160 +256,216 @@ function checkedOnClick(el) {
   }
   if (el.id == '3') {
     document.getElementById('pdadonovo').innerHTML =
-      '<p id="pdadonovo"><input type="date" id="novodado"></p>'
+      '<p id="pdadonovo"><input type="text" id="novodado"></p>'
     DadoparaAlterar = el.id
     possivelAlterar = true
   }
   if (el.id == '4') {
     document.getElementById('pdadonovo').innerHTML =
-      '<p id="pdadonovo"><input type="text" id="novodado"></p>'
+      '<p id="pdadonovo"><input type="number" id="novodado"></p>'
     DadoparaAlterar = el.id
     possivelAlterar = true
   }
   if (el.id == '5') {
     document.getElementById('pdadonovo').innerHTML =
-      '<p id="pdadonovo"><input type="number" id="novodado"></p>'
+      '<p id="pdadonovo"><input type="text" id="novodado"></p>'
     DadoparaAlterar = el.id
     possivelAlterar = true
   }
   if (el.id == '6') {
     document.getElementById('pdadonovo').innerHTML =
-      '<p id="pdadonovo"><input type="email" id="novodado"></p>'
+      '<p id="pdadonovo"><input type="text" id="novodado"></p>'
     DadoparaAlterar = el.id
     possivelAlterar = true
   }
   if (el.id == '7') {
     document.getElementById('pdadonovo').innerHTML =
-      '<p id="pdadonovo"><input type="number" id="novodado"></p>'
+      '<p id="pdadonovo"><input type="text" id="novodado"></p>'
+    DadoparaAlterar = el.id
+    possivelAlterar = true
+  }
+  if (el.id == '8') {
+    document.getElementById('pdadonovo').innerHTML =
+      '<p id="pdadonovo"><input type="text" id="novodado"></p>'
+    DadoparaAlterar = el.id
+    possivelAlterar = true
+  }
+  if (el.id == '9') {
+    document.getElementById('pdadonovo').innerHTML =
+      '<p id="pdadonovo"><input type="text" id="novodado"></p>'
+    DadoparaAlterar = el.id
+    possivelAlterar = true
+  }
+  if (el.id == '10') {
+    document.getElementById('pdadonovo').innerHTML =
+      '<p id="pdadonovo"><input type="text" id="novodado"></p>'
+    DadoparaAlterar = el.id
+    possivelAlterar = true
+  }
+  if (el.id == '11') {
+    document.getElementById('pdadonovo').innerHTML =
+      '<p id="pdadonovo"><input type="text" id="novodado"></p>'
+    DadoparaAlterar = el.id
+    possivelAlterar = true
+  }
+  if (el.id == '12') {
+    document.getElementById('pdadonovo').innerHTML =
+      '<p id="pdadonovo"><input type="text" id="novodado"></p>'
+    DadoparaAlterar = el.id
+    possivelAlterar = true
+  }
+  if (el.id == '13') {
+    document.getElementById('pdadonovo').innerHTML =
+      '<p id="pdadonovo"><input type="text" id="novodado"></p>'
+    DadoparaAlterar = el.id
+    possivelAlterar = true
+  }
+  if (el.id == '14') {
+    document.getElementById('pdadonovo').innerHTML =
+      '<p id="pdadonovo"><input type="text" id="novodado"></p>'
+    DadoparaAlterar = el.id
+    possivelAlterar = true
+  }
+  if (el.id == '15') {
+    document.getElementById('pdadonovo').innerHTML =
+      '<p id="pdadonovo"><input type="text" id="novodado"></p>'
+    DadoparaAlterar = el.id
+    possivelAlterar = true
+  }
+  if (el.id == '16') {
+    document.getElementById('pdadonovo').innerHTML =
+      '<p id="pdadonovo"><input type="text" id="novodado"></p>'
     DadoparaAlterar = el.id
     possivelAlterar = true
   }
 }
 
 function ConfirmarAlterar() {
-  let novo_dado = document.getElementById('novodado').value
+  let novodado = document.getElementById("novodado").value;
   if (possivelAlterar == true) {
+    alert('Imóvel alterado com sucesso !');
     if (DadoparaAlterar == '2') {
-      imoveis[ImoveisAlterando].description = novo_dado
+      imoveis[ImoveisAlterando].description = novodado;
     }
     if (DadoparaAlterar == '3') {
-      imoveis[ImoveisAlterando].rua = novo_dado
+      imoveis[ImoveisAlterando].rua = novodado;
     }
     if (DadoparaAlterar == '4') {
-      imoveis[ImoveisAlterando].num = novo_dado
+      imoveis[ImoveisAlterando].num = novodado;
     }
     if (DadoparaAlterar == '5') {
-      imoveis[ImoveisAlterando].bairro = novo_dado
+      imoveis[ImoveisAlterando].bairro = novodado;
     }
     if (DadoparaAlterar == '6') {
-      imoveis[ImoveisAlterando].city = novo_dado
+      imoveis[ImoveisAlterando].city = novodado;
     }
     if (DadoparaAlterar == '7') {
-      imoveis[ImoveisAlterando].UF = novo_dado
+      imoveis[ImoveisAlterando].UF = novodado;
     }
     if (DadoparaAlterar == '8') {
-      imoveis[ImoveisAlterando].pais = novo_dado
+      imoveis[ImoveisAlterando].pais = novodado;
     }
     if (DadoparaAlterar == '9') {
-      imoveis[ImoveisAlterando].cep = novo_dado
+      imoveis[ImoveisAlterando].cep = novodado;
     }
     if (DadoparaAlterar == '10') {
-      imoveis[ImoveisAlterando].tipo = novo_dado
+      imoveis[ImoveisAlterando].tipo = novodado;
     }
     if (DadoparaAlterar == '11') {
-      imoveis[ImoveisAlterando].garagens = novo_dado
+      imoveis[ImoveisAlterando].garagens = novodado;
     }
     if (DadoparaAlterar == '12') {
-      imoveis[ImoveisAlterando].suites = novo_dado
+      imoveis[ImoveisAlterando].suites = novodado;
     }
     if (DadoparaAlterar == '13') {
-      imoveis[ImoveisAlterando].quartos = novo_dado
+      imoveis[ImoveisAlterando].quartos = novodado;
     }
     if (DadoparaAlterar == '14') {
-      imoveis[ImoveisAlterando].seguranca = novo_dado
+      imoveis[ImoveisAlterando].seguranca = novodado;
     }
     if (DadoparaAlterar == '15') {
-      imoveis[ImoveisAlterando].preco = novo_dado
+      imoveis[ImoveisAlterando].preco = novodado;
     }
     if (DadoparaAlterar == '16') {
-      imoveis[ImoveisAlterando].condominio = novo_dado
+      imoveis[ImoveisAlterando].condominio = novodado;
     }
-    alert('Imovel alterado !')
   } else {
-    alert('N~so foi possivel altera !')
+    alert('Não foi possível alterar o imóvel !')
   }
 }
 
 function pesquisarexc() {
-  let codigoexcluir = document.getElementById('codigoexcluir').value
+  let codigoexcluir = document.getElementById("codigoexcluir").value
   let linhaCadastro =
-    '<table><tr><th>Código</td><th>Descrição</td>Rua<th>Número</td><th>Bairro</td><th>Cidade</td><th>Estado</td><th>País</td><th>CEP</td><th>Garagens</td><th>Suites</td></th>Suítes</td><th>Quartos</td><th>segurança</td><th>Preço</td><th>condominio</td></tr></td></tr>'
-
+  '<table><tr><th>Código</td><td>Descrição</td><td>Rua</td><td>Número</td><td>Bairro</td><td>Cidade</td><td>Estado</td><td>País</td><td>CEP</td><td>Garagens</td><td>Suites</td><td>Suítes</td><td>Quartos</td><td>segurança</td><td>Preço</td><td>condominio</td></th></tr>'
   if (imoveis.length > 0) {
-    Cadastro = BuscarCadastro(imoveis, codigoexcluir)
-    if (Cadastro != 1) {
+    Cadastro = BuscarCadastro(imoveis,codigoexcluir)
+    if (Cadastro != -1) {
       linhaCadastro +=
         '<tr><td>' +
-        imoveis[cadastro].codigo +
+        imoveis[Cadastro].codigo +
         '</td>' +
         '<td>' +
-        imoveis[cadastro].description +
+        imoveis[Cadastro].description +
         '</td>' +
         '<td>' +
-        imoveis[cadastro].rua +
+        imoveis[Cadastro].rua +
         '</td>' +
         '<td>' +
-        imoveis[cadastro].num +
+        imoveis[Cadastro].num +
         '</td>' +
         '<td>' +
-        imoveis[cadastro].bairro +
+        imoveis[Cadastro].bairro +
         '</td>' +
         '<td>' +
-        imoveis[cadastro].city +
+        imoveis[Cadastro].city +
         '</td>' +
         '<td>' +
-        imoveis[cadastro].UF +
+        imoveis[Cadastro].UF +
         '</td>' +
         '<td>' +
-        imoveis[cadastro].pais +
+        imoveis[Cadastro].pais +
         '</td>' +
         '<td>' +
-        imoveis[cadastro].cep +
+        imoveis[Cadastro].cep +
         '</td>' +
         '<td>' +
-        imoveis[cadastro].tipo +
+        imoveis[Cadastro].tipo +
         '</td>' +
         '<td>' +
-        imoveis[cadastro].garagens +
+        imoveis[Cadastro].garagens +
         '</td>' +
         '<td>' +
-        imoveis[cadastro].suites +
+        imoveis[Cadastro].suites +
         '</td>' +
         '<td>' +
-        imoveis[cadastro].quartos +
+        imoveis[Cadastro].quartos +
         '</td>' +
         '<td>' +
-        imoveis[cadastro].seguranca +
+        imoveis[Cadastro].seguranca +
         '</td>' +
         '<td>' +
-        imoveis[cadastro].preco +
+        imoveis[Cadastro].preco +
         '</td>' +
         '<td>' +
-        imoveis[cadastro].condominio +
+        imoveis[Cadastro].condominio +
         '</td></tr>'
       linhaCadastro += '</table>'
       document.getElementById('tabela5').innerHTML = linhaCadastro
     } else {
-      document.getElementById('tabela5').innerHTML = 'Imóvel não encontrado !'
+      document.getElementById('tabela5').innerHTML =
+        'Imóvel não foi encontrado '
     }
   } else {
-    document.getElementById('tabela5').innerHTML = 'Nenhum imóvel cadastrado !'
+    document.getElementById('tabela5').innerHTML =
+      'Nenhum imóvel foi cadastrado '
   }
 }
 
 function confirmarexc() {
-  let imoveisnovo = imovies.filter(excluir)
+  let imoveisnovo = imoveis.filter(excluir)
   imoveis = imoveisnovo
+  alert('Imóvel excluido com sucesso !')
 }
 
 function excluir(elemento) {
