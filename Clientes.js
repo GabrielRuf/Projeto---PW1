f0b1bc57024d3c5732c9c619f3f80a4ef08237fd
Gabriel Rufino <gabriel.rufino05@gmail.com>
2022-10-09 11:53:23 -0300

    feedback excluir

--- Clientes.js
+++ Clientes.js
@@ -233,6 +233,7 @@ function confirmarexc() // Usei o filter pra excluir, creio que é a função ma
 {
     let clientesnovo = clientes.filter(excluir);//Cria nova array que segue as condições da função excluir
     clientes = clientesnovo;
+    alert("Cliente excluido com sucesso!");
 }
 
 function excluir(elemento)
